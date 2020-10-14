da959921d649e78a14b1d91eddbb34b007cdfc66
Shlomo Zalman Heigh <shlomozalmanheigh@gmail.com>
2020-10-14 15:53:41 -0400

    Force iOS update
    This could be made into a config.xml option

--- src/ios/UpdateNotifierPlugin.swift
+++ src/ios/UpdateNotifierPlugin.swift
@@ -29,6 +29,7 @@ class UpdateNotifierPlugin : CDVPlugin {
 
     @objc internal func _didFinishLaunchingWithOptions(_ notification : NSNotification) {
         let siren = Siren.shared
+        siren.rulesManager = RulesManager(globalRules: .critical)
 
         if let countryCode = self.commandDelegate.settings["sirencountrycode"] as? String {
             siren.apiManager = APIManager(countryCode: countryCode)
